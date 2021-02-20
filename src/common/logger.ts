f766dd5a53c7af41bc81bee2e1a7b33da183b70f
刘阳 <chogath@163.com>
2021-02-20 10:39:45 +0800

    update: nest-logger

--- src/common/logger.ts
+++ src/common/logger.ts
@@ -3,24 +3,18 @@ import { LoggerService } from '@nestjs/common';
 export class NestLogger implements LoggerService {
 
   log(message: string, source?: string) {
-    console.log(message);
-    console.log(source);
+    console.log(`${message}\n [SOURCE]: ${source}`);
   }
   info(message: string, source?: string) {
-    console.log(message);
-    console.log(source);
+    console.info(`${message}\n [SOURCE]: ${source}`);
   }
   warn(message: string, source?: string) {
-    console.log(message);
-    console.log(source);
+    console.warn(`${message}\n [SOURCE]: ${source}`);
   }
   debug(message: string, source?: string) {
-    console.log(message);
-    console.log(source);
+    console.debug(`${message}\n [SOURCE]: ${source}`);
   }
-  error(message: string, source?: string, trace?: string) {
-    console.log(message);
-    console.log(source);
-    console.log(trace);
+  error(message: string, source?: string) {
+    console.error(`${message}\n [SOURCE]: ${source}`);
   }
 }
